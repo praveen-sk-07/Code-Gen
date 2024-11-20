b6bfeb311a579ce8b9c29a6fec7e425cabee29fe
S Praveen <124510097+praveen-sk-07@users.noreply.github.com>
2024-11-20 18:52:04 +0530

    Update login.php

--- login.php
+++ login.php
@@ -12,11 +12,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if ($user && password_verify($password, $user['password'])) {
         // Redirect to the desired URL after successful login
-        header("Location: https://code-deck.vercel.app/");
+        header("Location:https://code-gen-gamma.vercel.app/");
         exit(); // Stop executing the script after the redirect
     } else {
         // If login fails, you might want to send a response or redirect to an error page
         echo json_encode(['message' => 'Invalid credentials']);
     }
 }
-?>
+?> 
